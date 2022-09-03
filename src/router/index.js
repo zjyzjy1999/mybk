82fff0fe8893aeebab687577f68dddbf4d11a4ae
zjyzjy1999 <377158430@qq.com>
2022-09-03 23:20:08 +0800

    add

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,14 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import login from '../views/login'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'login',
+    component: login
   },
   {
     path: '/about',
---
+++ src/views/login/index.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="box">
+    <div class="bj" ref="vantaRef"></div>
+  </div>
+</template>
+<script>
+import * as THREE from 'three'
+// import BIRDS from 'vanta/src/vanta.birds'
+import CLOUDS from 'vanta/src/vanta.clouds'
+export  default  {
+  name:'login',
+  data(){
+    return{
+    }
+  },
+  methods:{
+
+  },
+  mounted() {
+    this.vantaEffect=CLOUDS({
+      el:this.$refs.vantaRef,
+      THREE:THREE
+    })
+  },
+  beforeDestroy() {
+    if(this.vantaEffect){
+      this.vantaEffect.destroy()
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+ .box{
+   width: 100%;
+   height: 100%;
+   display: flex;
+   .bj{
+     width: 100%;
+     height: 100%;
+   }
+ }
+
+</style>
